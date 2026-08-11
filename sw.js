4597fe1c5041519da14e2fa1641016f63d88385f
stanjarin <stanjarin@optusnet.com.au>
2026-08-11 11:14:49 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-const C='mw-v1',A=['./','./index.html','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='mw1000-cache-v1',A=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{let q=r.clone();caches.open(C).then(c=>c.put('./index.html',q));return r}).catch(()=>caches.match('./index.html')))}else{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))}});
